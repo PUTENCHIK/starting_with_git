578eb1ba6637e618a592e2f95feae1f184d74312
Maxim Olifirenko <opaolifirenkomaxim@gmail.com>
2024-09-25 12:50:12 +0800

    Added simple checking in file_b.php

--- file_b.php
+++ file_b.php
@@ -1,5 +1,21 @@
-<?php
+<?php 
 
-phpversion();
+modes = [
+	"default" => 1,
+	"premium" => 2,
+];
+
+app_mode = 3;
 
 ?>
+
+<?php if(app_mode === modes["default"]): ?>
+<h2>You are default user</h2>
+
+<?php elseif (app_mode === modes["premium"]): ?>
+<h1>Hello, Premium User!</h1>
+
+<?php else: ?>
+<p>Who are you?</p>
+
+<?php endif ?>
